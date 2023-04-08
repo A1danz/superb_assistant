c365e4bd29c5b438c8da58a75e054f77ad8b9375
liiceberg <2aysylu2003@gmail.com>
2023-04-08 14:14:31 +0300

    !!!

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    print_hi("!!!")
     print(f'Hi, superb-assistant')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
