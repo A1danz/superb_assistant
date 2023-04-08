d2b52f318dcafa42ec8a58cfe6d1e26ed596d1c3
liiceberg <2aysylu2003@gmail.com>
2023-04-08 17:18:17 +0300

    models 2.0

--- superb_assistant/main/models.py
+++ superb_assistant/main/models.py
@@ -4,34 +4,48 @@ from django.db import models
 
 
 class Student(User):
-    user_permissions = models.SmallIntegerField
+    user_permissions = models.SmallIntegerField('Права', default=0)
+
+    def __str__(self):
+        return self.first_name
 
 
 class Group(Group):
     code = models.CharField
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    title = models.CharField
+    title = models.CharField(max_length=100)
     text = models.TextField
     time = models.TimeField
 
 
 class Schedule(models.Model):
-    lessons_time = models.TimeField
-    lessons_on_monday = models.CharField
-    lessons_on_tuesday = models.CharField
-    lessons_on_wednesday = models.CharField
-    lessons_on_thursday = models.CharField
-    lessons_on_friday = models.CharField
-    lessons_on_saturday = models.CharField
+    lesson_time = models.TimeField
+    lesson_on_monday = models.CharField
+    lesson_on_tuesday = models.CharField
+    lesson_on_wednesday = models.CharField
+    lesson_on_thursday = models.CharField
+    lesson_on_friday = models.CharField
+    lesson_on_saturday = models.CharField
 
 
 class AttendanceLog(models.Model):
-    status = models.CharField(max_length=3)
+    ILL = "ill"
+    ABSENT = "abs"
+    BE = "be"
+    VALID_REASON = "vr"
+
+    STATUS = [
+        (ILL, 'Болеет'),
+        (ABSENT, 'Отсутствует'),
+        (VALID_REASON, 'Уважительная причина'),
+        (BE, 'Присутствует')
+    ]
+
+    status = models.CharField(max_length=3, choices=STATUS, default=BE)
     date = models.DateField
     lesson = models.TextField
-
-
-
-
