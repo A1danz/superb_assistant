810de2aa3980e28ea7d903bbc2f7114e5c62bec1
liiceberg <2aysylu2003@gmail.com>
2023-04-11 12:10:38 +0300

    доделаны модели

--- superb_assistant/main/admin.py
+++ superb_assistant/main/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Room, Student, Schedule, AttendanceLog, Post, StudyMaterial, Contact, Lesson
+from .models import Room, Student, AttendanceLog, Post, StudyMaterial, Contact, Lesson
 
 admin.site.register(Room)
 admin.site.register(Student)
-admin.site.register(Schedule)
 admin.site.register(Lesson)
 admin.site.register(AttendanceLog)
 admin.site.register(Post)
--- superb_assistant/main/models.py
+++ superb_assistant/main/models.py
@@ -3,19 +3,22 @@ import random
 import string
 
 from django.contrib.auth.models import User
-from django.contrib.auth.models import Group
 from django.db import models
+
 
 def rand():
     return ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10))
-#генерируем случайную строку
+
+
+# генерируем случайную строку
 
 class Room(models.Model):
-    code = models.CharField(max_length=10, unique=True, default = rand)
+    code = models.CharField(max_length=10, unique=True, default=rand)
     name = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.name
+
 
 class Student(models.Model):
     SIMPLE_STUDENT = 0
@@ -31,32 +34,19 @@ class Student(models.Model):
     permission = models.SmallIntegerField(choices=PERMISSION, default=SIMPLE_STUDENT)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField
-    last_name = models.CharField
 
-    def __str__(self):
-        return f'{self.last_name} {self.first_name}'
+    # def __str__(self):
+    #     return f'{self.last_name} {self.first_name}'
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    text = models.TextField
+    text = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
-
-
-class Schedule(models.Model):
-    lesson_time = models.TimeField
-    lesson_on_monday = models.CharField(max_length=50, blank=True)
-    lesson_on_tuesday = models.CharField(max_length=50, blank=True)
-    lesson_on_wednesday = models.CharField(max_length=50, blank=True)
-    lesson_on_thursday = models.CharField(max_length=50, blank=True)
-    lesson_on_friday = models.CharField(max_length=50, blank=True)
-    lesson_on_saturday = models.CharField(max_length=50, blank=True)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
 
 class Lesson(models.Model):
@@ -76,8 +66,8 @@ class Lesson(models.Model):
         (SATURDAY, 'Суббота')
     ]
     day = models.SmallIntegerField(choices=DAY)
-    start_time = models.TimeField
-    end_time = models.TimeField
+    start_time = models.TimeField()
+    end_time = models.TimeField()
     name = models.CharField(max_length=50, blank=True)
     schedule = models.ForeignKey(Room, on_delete=models.CASCADE)
 
@@ -107,7 +97,7 @@ class AttendanceLog(models.Model):
 
 class StudyMaterial(models.Model):
     name = models.CharField(max_length=50)
-    file = models.FileField
+    file = models.FileField()
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
 
     def __str__(self):
