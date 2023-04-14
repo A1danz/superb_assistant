76624d3ffd2f96dd271c46c62202a2e5fa93e7e0
liiceberg <2aysylu2003@gmail.com>
2023-04-14 21:59:36 +0300

    созданы шаблоны

--- superb_assistant/main/urls.py
+++ superb_assistant/main/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('attendance log', views.attend, name='attend'),
+    path('study materials', views.materials, name='materials'),
+    path('contacts', views.contacts, name='contacts'),
+    path('notice board', views.post, name='post'),
 ]
--- superb_assistant/main/views.py
+++ superb_assistant/main/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world :)")
-    # return render(request, "")
+    posts = Post.objects.all()
+    return render(request, "main/index.html", {'posts': posts})
+
+
+def contacts(request):
+    return render(request, "main/contacts.html")
+
+
+def post(request):
+    return HttpResponse("this page about posts")
+
+
+def materials(request):
+    return HttpResponse("this page about study materials")
+
+
+def attend(request):
+    return HttpResponse("this page about attendance log")
